e6b102514e848ddba4dca30f21a067b95de8e121
Christopher Metter <christopher.metter@informatik.uni-wuerzburg.de>
2015-12-11 12:03:32 +0100

    updated composer to neos 2.0

--- docker/install2nd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /data/www/itc28/Packages/Sites
-git clone git@github.com:lsinfo3/ITC.ITeleTraffic.Main.git Itc.ITeleTrafficMain
-
-#cd /data/www/itc28/
-#composer update
-#FLOW_CONTEXT=Production ./flow node:repair --node-type TYPO3.Neos.NodeTypes:Page --force && FLOW_CONTEXT=Production ./flow flow:cache:flush --force
